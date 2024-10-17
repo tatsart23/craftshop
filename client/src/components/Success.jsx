690b038292ef0322dc69f43657bbc3704d4f5e23
Aleksi Salonen <172624@sskky.fi>
2024-10-17 17:05:55 +0300

    Lisätty success sivu joka myös tyhjentää ostoskorin

---
+++ client/src/components/Success.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Success = ({ emptyCart }) => {
+    emptyCart()
+  return (
+    
+    <div>
+      <h1>Your payment was successfull!</h1>
+    </div>
+  )
+}
+
+export default Success
