192d32877028fb40d3b4c86fbf72816815bd0515
tatsart23 <Sartovuo@gmail.com>
2024-10-21 15:47:12 +0300

    karusellin tekoa

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { useAuth } from './AuthProvider'
+import React from "react";
+import { useAuth } from "./AuthProvider";
+import Carousel from "./Carousel";
+import { slides } from "../data/carouselData.json";
 
 const Home = () => {
   const auth = useAuth();
   return (
-    <div>
-      <h1>Home</h1>
-      
-    {auth.token ? (
-      <>
-        <p>Welcome {auth.user} </p>
-      </>
-    ) : (
-      <p>Welcome</p>
-    )}
+    <div className="home-container">
+      <h1 className="welcome-text">Home</h1>
 
-      
+      {auth.token ? (
+        <>
+          <p>Welcome {auth.user} </p>
+        </>
+      ) : (
+        <p>Welcome</p>
+      )}
 
-      
+      <Carousel data={slides} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
